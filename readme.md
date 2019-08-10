5dbe5011a7d794436a91d1eec69830f8920a4d4b
gg <gg@gmail.com>
2019-08-10 09:35:39 +0100

    easy todo list

--- readme.md
+++ readme.md
@@ -5,3 +5,50 @@
 - touch index.html webpack.config.js .babelrc
 - index.html `<div id="app-root"></div>`, `<script src="bundle.js"></script>`
 - npm i @babel/core @babel/preset-react @babel/preset-env @babel/preset-typescript webpack webpack-cli webpack-dev-server babel-loader react react-dom typescript @types/node @types/react @types/react-dom
+
+
+# React Interview Question and answers
+
+## Name Lifecycle Methods and their purpose
+16.4+
+
+phase 1. render
+
+- constructor
+  - only once setInitial state, this.state={}
+- static getDerviedStateFromProps
+  - componentWillReceiveProps, initial render and rerender phase, get derive state from the change in props
+  - why is a static method, you don't want user to access this keyword directly inside the method(no access to 'this')
+  - static method are actually class method not instance which means you know I do you cannot do like this dot some method name and so you cannot execute that so you cannot directly set state using this set state
+- render
+  - mandatory, cannot setState here otherwise you go in an infinite loop
+- componentDidMount
+  - 3 party dom ready make changes accordingly(相應地)
+
+phase 2. re-render
+- static getDerivedStateFromProps
+  - get your state from props
+- shouldComponentUpdate
+  - make a decision that do you really need to update, compare state then return true or false(no render)
+- render
+  - getSnapshotBeforeUpdate(componentWillUpdate), lazy loading
+- componentDidUpdate
+
+- componentWillUnmount
+  - die
+
+## why do we use arrow function in react? why and why not?
+you have to differentiate what is a method and what is a property
+arrow function don't have its own "this", lexical scope
+experimental syntax
+
+## how to prevent components from re-rendering.
+
+react hook
+uses
+useEffect
+hook
+
+2 phases when you on redner component
+- initRender
+- rerender
--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,78 @@
-import React from "react"
-import ReactDom from "react-dom"
+import React, {Fragment, useState} from 'react'
+import ReactDom from 'react-dom'
+import {Parent, Child} from './App'
+
+// refers another type, type can not extends
+type FormElem = React.FormEvent<HTMLFormElement>
+
+// new type, you can extends
+interface ITodo {
+  text: string
+  complete: boolean
+}
+
+interface ITodo2 extends ITodo {
+  tag: string[]
+}
 
 export default function App(): JSX.Element {
-  const sum = (a:number, b:number) : number => a + b
+  const [value, setValue] = useState<string>('')
+  const [todos, setTodos] = useState<ITodo[]>([])
+  const handleSubmit = (e: FormElem): void => {
+    e.preventDefault()
+    addTodo(value)
+    setValue('')
+  }
+  const addTodo = (text: string): void => {
+    const newTodos: ITodo[] = [...todos, {text, complete: false}]
+    setTodos(newTodos)
+  }
+  const completeTodo = (index: number): void => {
+    const newTodos: ITodo[] = [...todos]
+    newTodos[index].complete = !newTodos[index].complete
+    setTodos(newTodos)
+  }
+  const removeTodo = (index: number): void => {
+    const newTodos: ITodo[] = [...todos]
+    newTodos.splice(index, 1)
+    setTodos(newTodos)
+  }
   return (
-    <div>
-        <h1>Hello!! {sum(1, 2)}</h1> 
-    </div>
+    <Fragment>
+      <h1>Todo List</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={value}
+          onChange={e => setValue(e.target.value)}
+          required
+        />
+        <button type="submit">Add Todo</button>
+      </form>
+      <div>
+        {todos.map((todo: ITodo, index: number) => (
+          <Fragment key={index}>
+            <div>
+              <span
+                style={{textDecoration: todo.complete ? 'line-through' : null}}
+              >
+                {todo.text}
+              </span>
+              <button type="button" onClick={(): void => completeTodo(index)}>
+                {todo.complete ? 'incomplete' : 'Complete'}
+              </button>
+              <button type="button" onClick={(): void => removeTodo(index)}>
+                &times;
+              </button>
+            </div>
+          </Fragment>
+        ))}
+      </div>
+    </Fragment>
   )
 }
 
 const root = document.getElementById('app-root')
 
 ReactDom.render(<App />, root)
+
